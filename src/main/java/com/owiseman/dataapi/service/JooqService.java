d61b7bbeec6b16020436a2d0b2607611b52ce7a5
acproject <acproject@qq.com>
2025-01-23 22:49:46 +0800

    fix: 去掉多余的引用

--- src/main/java/com/owiseman/dataapi/service/JooqService.java
+++ src/main/java/com/owiseman/dataapi/service/JooqService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 import com.owiseman.jpa.util.TableAndDataUtil;
 import com.owiseman.jpa.model.DataRecord;
 
-import java.util.List;
-
 @Service
 
 public class JooqService {
