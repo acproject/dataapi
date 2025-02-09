cb75908b92db7c76a9a298c2b9ec04478563a011
acproject <acproject@qq.com>
2025-02-09 21:00:37 +0800

    fix: 简单处理了用户登录的审计

--- src/main/java/com/owiseman/dataapi/aop/audit/LoginAuditAspect.java
+++ src/main/java/com/owiseman/dataapi/aop/audit/LoginAuditAspect.java
@@ -3,18 +3,23 @@ package com.owiseman.dataapi.aop.audit;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoginAuditAspect {
-    @AfterReturning(pointcut = "execution(* org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider.authenticate(..))")
-    public void auditLoginSucess() {
-        // todo 记录登录成功日志
+    private static final Logger log = LoggerFactory.getLogger(LoginAuditAspect.class);
+    @AfterReturning(pointcut = "execution(* org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider.authenticate(..))",
+                    returning = "result")
+    public void auditLoginSucess(Object result) {
+        log.info("用户登录成功: {}", result);
     }
 
-     @AfterThrowing("execution(* org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider.authenticate(..))")
-    public void auditLoginFailure() {
-        // todo 记录登录失败日志
+     @AfterThrowing(value = "execution(* org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider.authenticate(..))",
+     throwing = "ex")
+    public void auditLoginFailure(Throwable ex) {
+        log.error("用户登录失败: ",  ex);
     }
 }
