c12edbab73619dc718a2d45f22e911bb454eb932
acproject <acproject@qq.com>
2025-02-10 21:09:35 +0800

    fix: 补齐deleteuser的API

--- src/main/java/com/owiseman/dataapi/controller/UserAdminController.java
+++ src/main/java/com/owiseman/dataapi/controller/UserAdminController.java
@@ -12,14 +12,7 @@ import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -123,5 +116,15 @@ public class UserAdminController {
         }
         roleService.assignRole(userId, request.getName(), token);
         return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable String userId,HttpServletRequest servletRequest) {
+        String authHeader = servletRequest.getHeader("Authorization");
+        String token = "";
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+        }
+        keycloakUserService.deleteUserById(userId, token);
     }
 }
